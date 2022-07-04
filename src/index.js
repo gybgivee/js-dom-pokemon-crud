6fb181dfdcaf17121b566777e5d284c96e4178a6
gybgivee <kmcn0512@gmail.com>
2022-07-04 17:14:46 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 const pokeForm = document.querySelector(".poke-form");
 const pokeList = document.querySelector(".poke-list");
+let state = [];
 
+function init() {
+
+  setPokemonsToState();
+
+}
+function setPokemonsToState() {
+  get();
+}
+function setstate(updatedState) {
+  Object.keys(updatedState).forEach((prop) => {
+    state[prop] = updatedState[prop];
+  });
+
+  rander();
+}
+function rander() {
+  listenToAddPokemonForm();
+}
 function addPokemon(pokemon) {
   const liEl = document.createElement("li");
+
   const imgEl = document.createElement("img");
   const h2El = document.createElement("h2");
 
@@ -13,6 +33,26 @@ function addPokemon(pokemon) {
 
   liEl.append(imgEl, h2El);
   pokeList.append(liEl);
+
+
+  const linkDelete = document.createElement("a");
+  liEl.appendChild(linkDelete);
+  linkDelete.innerHTML = "Delete";
+  linkDelete.setAttribute("href", "#");
+  linkDelete.setAttribute("class", "delete");
+  linkDelete.setAttribute("id", "delete_" + pokemon.id);
+  listenToRemovePokemon(linkDelete);
+
+
+  const linkLike = document.createElement("a");
+  liEl.appendChild(linkLike);
+  linkLike.innerHTML = "Like";
+  linkLike.setAttribute("href", "#");
+  linkLike.setAttribute("class", "update");
+  linkLike.setAttribute("id", "update_" + pokemon.id);
+  listenToUpdateLikePokemon(linkLike);
+
+
 }
 
 function addPokemons(pokemons) {
@@ -24,32 +64,97 @@ function listenToAddPokemonForm() {
     event.preventDefault();
     const pokemon = {
       name: pokeForm.name.value,
-      image: pokeForm.image.value
+      image: pokeForm.image.value,
+      like: false,
     };
 
-    // CREATE
-    // fetch("http://localhost:3000/pokemons", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(pokemon)
-    // })
-    //   .then(res =>  res.json())
-    //   .then(pokemon => addPokemon(pokemon));
-    //   });
+    create(pokemon);
 
     pokeForm.reset();
   });
 }
+function listenToRemovePokemon(linkDelete) {
+  linkDelete.addEventListener("click", function (event) {
 
-function init() {
-  listenToAddPokemonForm();
+    const id = event.target.id.replace("delete_", "");
+    console.log('Remove click', id)
+    deletePokemon(id);
+  })
 
-  // READ
-  // fetch("http://localhost:3000/pokemons")
-  //   .then(res => res.json());
-  //   .then(pokemons => addPokemons(pokemons));
+
+
 }
+function listenToUpdateLikePokemon(linkLike) {
+  linkLike.addEventListener("click", function (event) {
+    const liEl = document.querySelectorAll(".pokemon");
+    linkLike.innerHTML = 'Liked';
+    linkLike.style.color = '#C71585';
+    liEl.appendChild(linkLike);
+
+    const id = event.target.id.replace("update_", "");
+    console.log('update click', id)
+
+    update(id);
+
+  })
+};
+
+
+const get = () => {
+  fetch('http://localhost:3000/pokemons')
+    .then(function (response) {
+      console.log('get response', response);
+      return response.json();
+    })
+    .then(function (data) {
+      setstate(data);
+      addPokemons(data);
+    });
+
+};
+const create = (pokemon) => {
+  fetch('http://localhost:3000/pokemons', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(pokemon)
+
+
+  }).then(function (response) {
+    console.log('create response', response);
+    return response.json();
+  })
+};
+const update = (id) => {
+  fetch(`http://localhost:3000/pokemons/${id}`, {
+    method: "PATCH",
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      like: true
+    })
+  }).then(function (response) {
+
+    return response.json();
+  }).then(function (data) {
+
+    setstate(data);
+
+  });
+
+};
+const deletePokemon = (id) => {
+  fetch(`http://localhost:3000/pokemons/${id}`, {
+    method: 'DELETE'
+  })
+    .then(function (response) { return response.json() })
+    .then(function (data) {
+      setstate(data);
+    })
+
+};
+
 
 init();
